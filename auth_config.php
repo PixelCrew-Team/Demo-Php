29acddd669a880cd43b4e8ffafc89df58c712a55
PixelCrew Team <pixelcrewteam@gmail.com>
2025-10-29 10:45:44 -0400

    Actualizar auth_config.php

--- auth_config.php
+++ auth_config.php
@@ -1,3 +1,5 @@
+// Nota: Este archivo es muy importante ya que es el que maneja y crea la base de datos facilitando a que no uses una base de datos de mongoDb u otra plataformas.
+
 <?php
 // auth_config.php - Configuración del sistema de autenticación
 
